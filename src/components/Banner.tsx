6f7f86ed1760891fd0848a31380723b7384c0a9c
kevin <kevingitabr@gmail.com>
2024-07-11 08:53:12 +0530

    base structure

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,9 @@
 export const Banner = () => {
-  return null;
+  return (
+    <div className="py-3 text-center">
+      <div className="container">
+        <a href="#" className="underline underline-offset-2 font-medium">Explore the demo</a>
+      </div>
+    </div>
+  );
 };
