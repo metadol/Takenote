a9c79d80d73450c0faa8e320683953a71c4c4629
kevin <kevingitabr@gmail.com>
2024-07-16 14:00:53 +0530

    completed banner and hero sections

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,12 @@
 export const Banner = () => {
+
   return (
-    <div className="py-3 text-center">
+    <div className="py-3 text-center bg-[linear-gradient(to_right,#FCD6FF,#29DFFF,#FFFD80,#F89ABF,#FCD6FF)]">
       <div className="container">
-        <a href="#" className="underline underline-offset-2 font-medium">Explore the demo</a>
+        <p className="font-medium">
+          <span className="hidden sm:inline">Introducing a completely redesigned interface —{" "}</span>
+          <a href="#" className="underline underline-offset-2">Explore the demo</a>
+        </p>
       </div>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,31 @@
+import Image from 'next/image';
+import LogoImage from '../assets/images/logosaas.png'
+import MenuIcon from '../assets/icons/menu.svg'
+
 export const Navbar = () => {
-  return null;
+  return (
+    <div className='bg-black px-4'>
+        <div className='py-4 flex items-center justify-between'>
+          
+          <div className='relative'>
+            <div className='absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#FCD6FF,#29DFFF,#FFFD80,#F89ABF,#FCD6FF)] blur-md' />
+            <Image src={LogoImage} alt='saas-logo' className='h-12 w-12 relative' />
+          </div>
+
+          <div className='border border-white border-opacity-30 h-10 w-10 inline-flex justify-center items-center rounded-lg sm:hidden'>
+            <MenuIcon className='text-white' />
+          </div>
+
+          <nav className='gap-6 items-center hidden sm:flex'>
+            <a className='text-white text-opacity-60 hover:text-opacity-100 transiton'>About</a>
+            <a className='text-white text-opacity-60 hover:text-opacity-100 transiton'>Features</a>
+            <a className='text-white text-opacity-60 hover:text-opacity-100 transiton'>Updates</a>
+            <a className='text-white text-opacity-60 hover:text-opacity-100 transiton'>Help</a>
+            <a className='text-white text-opacity-60 hover:text-opacity-100 transiton'>Customers</a>
+            <button className='bg-white py-2 px-4 rounded-lg'>Get for free</button>
+          </nav>
+
+        </div>
+    </div>
+  );
 };
